2e31ca1d4180ea3afccc32c9a370fe984fb5bcb1
Tanya Bulaeva <t.akberdina@gmail.com>
2023-11-29 18:56:21 +0500

    mainPage

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import { MainPage } from './pages/mainPage/Main';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
-    </div>
+    <MainPage />
+
   );
 }
 
