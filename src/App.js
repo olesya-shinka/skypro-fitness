3f24d553fc332e42aa816c1d157c3a654babc441
Olesya Shinka <129954640+ol-shinka@users.noreply.github.com>
2023-11-30 13:14:36 +0500

    написала стили, создала компоненты

--- src/App.js
+++ src/App.js
@@ -1,17 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Workout from "./pages/Workout/WorkoutPage";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
-    </div>
-  );
+  return <Workout />;
 }
 
 export default App;
---
+++ src/pages/Workout/WorkoutPage.js
@@ -0,0 +1,61 @@
+import * as S from "./styles";
+
+const Workout = () => {
+  return (
+    <S.Container>
+      <S.Header></S.Header>
+      <S.Main>
+        <S.Heading>Йога</S.Heading>
+        <S.Title>Красота и здоровье / Йога на каждый день / 2 день</S.Title>
+        <S.Player>
+          url="https://www.youtube.com/embed/oqe98Dxivns"
+        </S.Player>
+        <S.Exercises>
+          <S.ContentExercises>
+            <S.TitleExercises>Упражнения</S.TitleExercises>
+            <S.ListExercises>
+              <S.ListExercisesItem>
+                Наклон вперед (10 повторений) 
+                Наклон назад (10 повторений)
+                Поднятие ног, согнутых в коленях (5 повторений)
+              </S.ListExercisesItem>
+            </S.ListExercises>
+            <S.ButtonProgress>Заполнить свой прогресс</S.ButtonProgress>
+          </S.ContentExercises>
+          <S.Progress>
+            <S.TitleProgress>Мой прогресс по тренировке:</S.TitleProgress>
+            <S.ListProgress>проценты</S.ListProgress>
+          </S.Progress>
+        </S.Exercises>
+      </S.Main>
+      <S.Wrapper>
+        <S.Modal>gfd</S.Modal>
+        <S.ModalClose />
+      </S.Wrapper>
+      <S.FormModal>
+        <S.TitleModal>Мой прогресс</S.TitleModal>
+        <S.InputsModal>
+          <S.InputsText>
+            Сколько раз вы сделали упражнение?
+            <S.Input>
+              Введите значение
+              <S.Message>gfdgfdgdfg</S.Message>
+            </S.Input>
+          </S.InputsText>
+        </S.InputsModal>
+        <S.ButtonProgress>Отправить</S.ButtonProgress>
+      </S.FormModal>
+      <S.Wrapper>
+        <S.Modal>
+          <S.ModalClose>
+            <S.ContentSuccess>
+              <S.SuccessText>Ваш прогресс засчитан!</S.SuccessText>
+            </S.ContentSuccess>
+          </S.ModalClose>
+        </S.Modal>
+      </S.Wrapper>
+    </S.Container>
+  );
+};
+
+export default Workout;
