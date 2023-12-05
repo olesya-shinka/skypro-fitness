07fd774900e16148c3ffcbe0acae59188bc03c38
Tanya Bulaeva <t.akberdina@gmail.com>
2023-12-05 21:33:59 +0500

    роуты испр

--- src/routes.js
+++ src/routes.js
@@ -7,21 +7,19 @@ import WorkoutPage from "./pages/Workout/WorkoutPage";
 import { ProtectedRoute } from "./components/ProtectedRoute/ProtectedRoute";
 import SignInPage from "./pages/log-in/login";
 
-export const AppRoutes = ({ user }) => {
+export const AppRoutes = ({user}) => {
     return (
       <Routes>
-        <Route path="/" element={<MainPage />} />
-        <Route path="/CourseInfo" element={<CourseInfo />} />
         <Route path="/Login" element={<SignInPage />} />
         <Route path="/SignUp" element={<SignUpPage />} />
-        <Route path="/PersonalPage" element={
-          <ProtectedRoute isAllowed={Boolean(user)}>
-            <PersonalPage />
-          </ProtectedRoute>
-        }
-      />
-    
-        <Route path="/WorkoutPage" element={<WorkoutPage />} />
+        <Route element={<ProtectedRoute isAllowed={Boolean(user)} />}>
+          <Route path="/" element={<MainPage />} />
+          <Route path="CourseInfo" element={<CourseInfo />} />
+          <Route path="PersonalPage" element={<PersonalPage />} />
+          <Route path="/WorkoutPage" element={<WorkoutPage />} />
+      </Route>
+  
+
         
       </Routes>
     );
