3339fb495715a4ccf4f06db1b3b2521d85cc684d
Tanya Bulaeva <t.akberdina@gmail.com>
2023-12-06 14:46:46 +0500

    испр орф ошибка

--- src/firebase.js
+++ src/firebase.js
@@ -10,7 +10,7 @@ const firebaseConfig = {
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASEURL,
 };
 
 // Initialize Firebase
