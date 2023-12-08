9bbec8cca9529fd20895bee4227c5f84ac426239
Olesya Shinka <129954640+ol-shinka@users.noreply.github.com>
2023-12-08 12:19:38 +0500

    +

--- src/store/courseActions.js
+++ src/store/courseActions.js
@@ -3,7 +3,7 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 
 export const loadCourses = createAsyncThunk(
   "courses/all",
-  async (_, { extra: { apiClient, api }, rejectWithValue }) => {
+  async (_, { extra: {  databaseURL, api }, rejectWithValue }) => {
     try {
       const config = {
         headers: {
@@ -11,7 +11,7 @@ export const loadCourses = createAsyncThunk(
         },
         skipAuth: true
       };
-      const response = await apiClient.get(api.COURSES, config);
+      const response = await  databaseURL.get(api.COURSES, config);
       if (response.statusText !== "OK") {
         throw new Error("Что-то пошло не так");
       }
