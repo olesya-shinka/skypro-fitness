2f9a1d96d38eeb9cbcba5cf245106b06e5de2ae9
Sofia <129661856+YakovlevaSS@users.noreply.github.com>
2023-12-10 22:51:13 +0300

    store

--- src/store/store.jsx
+++ src/store/store.jsx
@@ -1,25 +1,13 @@
-import { configureStore } from "@reduxjs/toolkit";
-import { setupListeners } from "@reduxjs/toolkit/query";
-import { workoutApi } from "../store/slices/workoutsSlice";
-import loadingReducer from "./slices/loadingSlice";
-import selectedWorkoutReducer from "./slices/selectWorkoutSlice";
-import userCoursesReducer from "./slices/userCoursesSlice";
-import workoutsReducer from "./slices/workoutsSlice";
-import selectedTrainingReducer from "./slices/selectTrainSlice";
+import {configureStore} from "@reduxjs/toolkit";
 import authReducer from "./slices/userSlice";
+import { localStorageMiddleware } from "./slices/userSlice";
 
 export const store = configureStore({
-  reducer: {
-    [workoutApi.reducerPath]: workoutApi.reducer,
-    user: authReducer,
-    loading: loadingReducer,
-    selectedWorkout: selectedWorkoutReducer,
-    userCourses: userCoursesReducer,
-    workouts: workoutsReducer,
-    selectedTraining: selectedTrainingReducer,
-  },
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(workoutApi.middleware),
-});
+    reducer: {
+        user: authReducer,
+    },
 
-setupListeners(store.dispatch);
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(localStorageMiddleware),
+
+})
