01e710de449f0f6bcd7e36254c3ec84a0341b8dd
Sofia <129661856+YakovlevaSS@users.noreply.github.com>
2023-12-11 17:34:02 +0300

    fixed routes

--- src/App.js
+++ src/App.js
@@ -3,11 +3,20 @@
 /* eslint-disable prettier/prettier */
 
 import { AppRoutes } from "./routes.js";
-import { useState } from "react";
-import React from "react";
+import { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+
+import "./firebase";
+import { initializeUserFromLocalStorage } from "./store/slices/userSlice.js";
 
 function App() {
   const [user, setUser] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    // При монтировании компонента, диспетчеризуем действие для инициализации из localStorage
+    dispatch(initializeUserFromLocalStorage());
+  }, [dispatch]);
 
   return (
     <div>
