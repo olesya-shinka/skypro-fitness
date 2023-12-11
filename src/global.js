3b4c3eb9e909d20dd01f8c912bb5c9dcf29816c0
Sofia <129661856+YakovlevaSS@users.noreply.github.com>
2023-12-11 16:34:38 +0300

    del aii global style

--- src/global.js
+++ src/global.js
@@ -1,14 +1,6 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
-body {
-  margin: 0;
-  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
-    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
-    sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-}
 
 code {
   font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
@@ -55,7 +47,6 @@ ul li {
     height: 100%;
     font-family: StratosSkyeng, sans-serif;
     font-family: "StratosSkyeng", sans-serif;
-    color: #ffffff;
   }
 
   `
--- src/pages/sign-up/signupStyle.js
+++ src/pages/sign-up/signupStyle.js
@@ -1,21 +1,4 @@
-import styled, { createGlobalStyle } from "styled-components";
-
-export const GlobalStyle = createGlobalStyle`
-  * {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-  }
-
-  @font-face {
-    font-family: "StratosSkyeng", sans-serif;
-    src: local("StratosSkyeng"), local("StratosSkyeng"),
-      url("../../../public/fonts/StratosSkyeng.woff2") format("woff2"),
-      url("../../../public/fonts/StratosSkyeng.woff") format("woff");
-    font-weight: 400;
-    font-style: normal;
-  } 
-`;
+import styled from "styled-components";
   
 export const Login = styled.div`
     display: flex;
