95de280241941b643512299d39482bc9a959b1c9
Olesya Shinka <129954640+ol-shinka@users.noreply.github.com>
2023-12-18 19:41:06 +0500

    -eslint

--- src/components/LayoutModal/ProgressModal/ProgressModal.jsx
+++ src/components/LayoutModal/ProgressModal/ProgressModal.jsx
@@ -1,16 +1,11 @@
-/* eslint-disable prettier/prettier */
-/* eslint-disable react/prop-types */
-/* eslint-disable prettier/prettier */
 /* eslint-disable no-unused-vars */
-/* eslint-disable prettier/prettier */
 import * as S from "./styles";
 import { useForm } from "react-hook-form";
 import { useSelector, useDispatch } from "react-redux";
 import ButtonMain from "../../ButtonMain/ButtonMain";
-import { addProgress } from "../../../Api";
 import { idSelector } from "../../../store/selectors/user";
 import InputProgress from "../InputProgress/InputProgress";
-import { getUserProgress, postProgress, getProgress } from "../../../Api";
+import { getUserProgress, addProgress, postProgress, getProgress } from "../../../Api";
 import { courseList } from "../../../store/selectors/coursesNew";
 import { userProgress } from "../../../store/selectors/progress";
 
