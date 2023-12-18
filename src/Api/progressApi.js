10be66ed6763fc3caddc53aa33cc8d3d1d751d02
Sofia <129661856+YakovlevaSS@users.noreply.github.com>
2023-12-19 00:28:55 +0300

    второй вариант

--- src/Api/progressApi.js
+++ src/Api/progressApi.js
@@ -17,3 +17,28 @@ export const updateProgress = async (userId, workoutId, exerciseNumber, progress
     console.error("Ошибка при обновлении прогресса:", error);
   }
 };
+
+//второй вариант
+// import { ref, get, update } from "firebase/database";
+// import { getDatabase } from "firebase/database";
+
+// export const updateProgress = async (userId, workoutId, exerciseNumber, progress) => {
+//   const db = getDatabase();
+//   try {
+//     const exerciseId = `${workoutId}_exercise${exerciseNumber}`;
+
+//     // Ссылка на документ в коллекции "userProgress"
+//     const progressRef = ref(db, "userProgress", userId, "exercises", exerciseId);
+//     console.log("Trying to update progress:", userId, workoutId, exerciseNumber, progress);
+
+//     const snapshot = await get(progressRef);
+//     const progressFirebase = snapshot.val();
+
+//     // Добавляем или обновляем прогресс
+//     await update(progressRef, progressFirebase);
+
+//     console.log("Прогресс обновлен успешно!");
+//   } catch (error) {
+//     console.error("Ошибка при обновлении прогресса:", error);
+//   }
+// };
